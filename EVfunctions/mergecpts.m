109d87a70cad9c9b2f9668a8a0bc4035457f1ddf
PaulFackler <paul_fackler@ncsu.edu>
2018-12-10 13:08:18 -0500

    revised to conform with new syntax for EVcreate and to handle models with noise terms

--- EVfunctions/mergecpts.m
+++ EVfunctions/mergecpts.m
@@ -1,58 +1,74 @@
 % mergecpts Merges the CPTs for a set of variables
 % USAGE
-%   [P1,parents1]=mergecpts(P0,X,parents0);
+%   [P,parents]=mergecpts(p,parents,X,e,eremove,options);
 % INPUTS
-%   P0       : p-element cell array of CPT matrices for individual variables
+%   p        : m-element cell array of CPT matrices for individual variables
+%   parents  : m-element cell array of parent vectors (these must be row vectors)
 %   X        : nx x dx matrix of values of conditioning variables
-%   parents0 : p-element cell array of parent vectors (these must be row vectors)
+%   e        : de-element cell array of rv structures (discrete or 
+%                w/ discrete approximations)
+%   eremove  : de-vector specifying which of the noise variables are summed out
+%   options  : structure variable (fields described below)
 % OUTPUTS
-%   P1       : CPT matrix for combined variables
-%   parents1 : index vector for the parents of the combined variables
+%   P        : CPT matrix for combined variables
+%   parentsc : index vector for the parents of the combined variables
 %
 % The parent vectors contain a set of unique integers on {1,...,dx} that
 %   indicate the columns of X associated with the conditioning variables for
 %   each variable being combined. These indices must correspond to the way that
 %   the conditioning variables are combined in the associated CPT (hence 
 %   they need not be in  sorted order).
-% P1 and parents1 are always output in sorted order.
-%
-% This procedure expands each CPT to the full output size and uses
-% kroncol to combine CPTs
+% The presense of noise variables is indicated be negative values in the
+%   parent vectors. Any noise variables that are not summed out will also
+%   enter the P matrix in sorted order.
+% P and parentsc are always output in sorted order.
 
-function [P1,parents1]=mergecpts(P0,X,parents0,options)
-if nargin<4, options=[]; end
-parents1=unique([parents0{:}]);
-if isempty(X)
-  if isempty(options)
-    error('an options structure must be passed is X is empty')
-  end
-  if isfield(options,'nx')
-    d=length(options.nx);
-    options.nx=options.nx(parents1);
-  elseif isfield(options,'maxX')
-    d=length(options.maxX);
-    options.maxX=options.maxX(parents1);
+function  [P,parentsc] = mergecpts(p,parents,X,e,eremove)
+% get information about noise variables (e)
+if nargin<4
+  e={};
+  de=0;
+else
+  de = length(e);
+end
+if nargin<5 || isempty(eremove), eremove=false(de,1); end
+if length(eremove) == 1, eremove = repmat(eremove,de,1); end
+[eenter,eexit] = getenterexit(parents,e);
+[Ip,Iy] = EVindices(parents,X,e,eremove);
+
+m=length(p);
+if m>1
+  parentsc=unique([parents{:}]);
+  parentsc(parentsc<0) = sort(parentsc(parentsc<0),'descend');
+else
+  parentsc=parents{1};
+  parentsc=[sort(parentsc(parentsc<0),'descend') parentsc(parentsc>0)];
+end
+w=cellfun(@(x)x.cpt,e,'UniformOutput',false);
+for i=1:m
+  if i==1
+    if isempty(Ip{1})
+      P=p{1};
+    else
+      P=p{1}(:,Ip{1});
+    end
   else
-    error('cannot determine the size of X')
+    P = kroncol(P(:,Iy{i}),p{i}(:,Ip{i}));
   end
-else
-  d=size(X,2);
+  % sum out noise variables as appropriate
+  if ~isempty(e)
+    if any(eremove & eexit==i)
+      ii=find(eremove & eexit==i);
+      wi = w{ii(1)};
+      for j=2:length(ii)
+        wi = w{ii(j)}*wi';  
+        wi=wi(:);
+      end
+      P=reshape(reshape(P,[],length(wi))*wi,size(P,1),[]); 
+      % remove summed noise terms from parent list
+      parentsc(matchindices(-ii,parentsc)) = [];
+    end
+  end
 end
+return
 
-if length(parents1)==d
-  X1=X;
-elseif ~isempty(X)
-  X1=unique(X(:,parents1),'rows');
-else
-  X1=[];
-end
-% create function to list the columns of X1 associated with a parent vector
-var=zeros(1,d);  var(parents1)=1:length(parents1); 
-pind=@(i) var(parents0{i});
-p=length(P0);
-ind=getI(X1,pind(1),options);
-P1=P0{1}(:,ind);
-for i=2:p
-  ind=getI(X1,pind(i),options);
-  P1=kroncol(P1,P0{i}(:,ind));
-end
